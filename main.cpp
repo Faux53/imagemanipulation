4e217eef2fbc2601e2c2d615ae26af6084032837
Andrew Adams <aadams9900@gmail.com>
2018-05-23 17:50:53 -0700

    multithreaded
    multithread implementation.
    
    TODO:  multiple images in folders

--- main.cpp
+++ main.cpp
@@ -8,50 +8,71 @@
 #include </Users/andrewadams/opencv/modules/imgproc/include/opencv2/imgproc.hpp>
 #include </Users/andrewadams/opencv/modules/imgproc/include/opencv2/imgproc/imgproc_c.h>
 #include </Users/andrewadams/opencv/modules/imgcodecs/include/opencv2/imgcodecs/imgcodecs_c.h>
+#include <atomic>
+#include <thread>
+#include </Users/andrewadams/Desktop/Pinto/sloth_comp_read.cpp/cpp-sockets/net/serversocket.hpp>
+
 
 using namespace cv;
 using namespace std;
 
 
+static bool s_finished = false;  //declare the thread as in process
 
-int main( int argc, char** argv)
-{
-    
-    char const *i = "/Users/andrewadams/Desktop/pinto/sloth_comp_read.cpp/sloth_comp_read.cpp/images.jpg";
-    //grabs the document from the root file, it's also possible to just include your image directory
-    
+
+void read_comp() {
+    char const *i = "/Users/andrewadams/Desktop/pinto/sloth_comp_read.cpp/sloth_comp_read.cpp/images.jpg"; //grabs the image from the root file, it's also possible to just include your image directory
+
     Mat read_image;
     read_image = imread(i, cv::IMREAD_COLOR);
     // Call the source and apply your changes to the image using OpenCV (i.e. greyscale, tracking, rotating etc etc)
     
     
-    
     if(! read_image.data )                              // Check for invalid input
     {
         cout <<  "No sloths in these woods" << std::endl ;
-        return -1;
+        return;
     }
-    
-    
-    namedWindow( "Display window", WINDOW_AUTOSIZE );    // Create a window for display.
-    imshow( "Display window", read_image );              // Show our image inside it.
-    
     
     Mat compress;
     int p[3];  //writes out parameters within the compress Mat
     p[0] = IMWRITE_JPEG_QUALITY;
     p[1] = 50; //decide on quality ranging from 0-->100
     compress = imread(i, p[1]);  //apply the changes you made to your images quality by calling the char const up above.
-    
+ 
     
     Mat save;
     save = imwrite("/Users/andrewadams/Desktop/pinto/sloth_comp_read.cpp/sloth_comp_read.cpp/out.jpg", compress); //saves the compression above
+    
+    while (!s_finished) {
+        std::cout << "working... \n";
+        std::this_thread::sleep_for(1s);
+    }
+    
+};
+
+
+
+
+int main( int argc, char** argv)
+{
+    
+    std::thread worker(read_comp);
+    
+    
+    std::cin.get();
+    s_finished = true;
+    
+    worker.join();
+    std::cout << "Finished" << std::endl;
+    
+    std::cin.get();
+    
     
   
     exit(0);
 }
 
-#include </Users/andrewadams/Desktop/Pinto/sloth_comp_read.cpp/cpp-sockets/net/serversocket.hpp>
 
 net::serversocket::~serversocket() {
     close();
